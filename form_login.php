2a8ab2db17ecf7018ce3bcc13508d8b87e9c6bcf
Adam Idham Ramadhan <adamramadhan791@gmail.com>
2022-03-29 22:48:07 +0700

    Tugas 4 PHP

---
+++ form_login.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <title>Form Login</title>
+    <style>
+      body {
+        background-color: skyblue;
+      }
+      .box {
+      width: 300px;
+      background: white;
+      margin: 100px auto;
+      padding: 20px;
+      border-radius: 10px;
+      }
+      thead{
+          background-color: #45a049;
+          color: white;
+      }
+      input[type=text],[type=password]{
+          width: 100%;
+          padding: 12px 20px;
+          margin: 8px 0;
+          display: inline-block;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+          box-sizing: border-box;
+      }
+
+      input[type=submit]{
+          width: 30%;
+          background-color: #4CAF50;
+          color: white;
+          padding: 5px 5px;
+          margin: 5px 0;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
+      }
+      input[type=submit],[type=reset]:hover {
+          background-color: #45a049;
+      }
+    </style>
+  </head>
+  <body>
+  <?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    function cekLogin($username, $password) {
+      if ($username == 'admin' && $password == 'mimin') {
+        header('Location: home.php');
+        exit();
+      }
+    }
+    ?>
+  <form action="#" method="POST">
+            <div class="box">
+            <table align="center" cellpadding="10"  cellspacing="0">
+                <thead><tr><th colspan="2">Sign in</th></tr></thead>
+                <tbody>
+                    <tr>
+                        <td width="30%">Username</td>
+                        <td><input type="text" name="username" id="username" placeholder="Masukkan Username" required/></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="password" id="password" placeholder="Masukkan Password" required /></td>
+                    </tr>
+                </tbody>
+                <tfoot><tr><th colspan="2" >
+                    <input type="submit" name="login" value="Submit"><?= cekLogin($username, $password) ?>
+                </th></tr></tfoot>
+            </table>
+            </div>
+        </form>
+  </body>
+</html>
+
+
+
+
---
+++ home.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile Saya</title>
+        <link rel="shorcut icon" href="https://www.nurulfikri.com/wp-content/uploads/2018/12/800px-Deepin_logo.svg_.png" />
+        <link rel="stylesheet" type="text/css" href="styleku.css" />
+        <style>
+          :root {
+          /* * = universal (keseluruhan) */
+          font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+          color: black;
+          font-size: 12pt;
+          line-height: 0.5cm;
+          }
+          h1{
+              color: black;
+              line-height: 1cm;
+              text-transform: uppercase;
+              letter-spacing: 5px;
+          }
+          h3{
+              color: black;
+              text-align: center;
+          }
+          fieldset, legend{ /* grouping */
+              background: linear-gradient(to bottom right, #00ffcc -1%, #ffffff 101%);
+              color: darkblue;
+              border: 2px solid black;
+              border-top-right-radius: 20px;
+              border-bottom-left-radius: 20px;
+              padding: 8px 20px;
+          }
+          body{
+              margin : 0.5cm 2cm;
+              background: linear-gradient(to bottom right, #ccffff -1%, #ffffff 101%);
+          }
+          hr{
+              border: 1px solid maroon;
+              margin-bottom: 20px;
+              border-width: 2px;
+          }
+          p{
+              text-align: justify;
+              color: darkblue;
+              font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+          }
+        </style>
+      </head>
+    <body>
+        <h1>
+            <marquee behavior="alternate" scrolldelay="100">
+                Welcome To Adam Idham Ramadhan'<sup>s</sup> SITE
+            </marquee>
+        </h1>
+        <hr/>
+        <fieldset>
+            <legend><h2>Profile Saya</h2></legend>
+        <img src="https://j.top4top.io/p_2231ijfnt1.jpeg" alt="Foto Saya" align="left" border="2" width="5%" hspace="15">
+        
+        <p >Perkenalkan nama saya Adam Idham Ramadhan, saya kuliah di Universitas Bengkulu jurusan Informatikan angkatan 2019. saya berkeinginan untuk menjadi seorang web developer. seorang web developer memliki tugas yaitu untuk mengembangkan, menciptakan, dan memodifikasi suatu web. Menganalisa kebutuhan pengguna, merancang, dan mengembangkan web.</p>
+        <p >Alasan kenapa saya ingin menjadi web developer yang pertama, peluang kerja web developer terbuka sangat lebar. Di zaman serba digital seperti sekarang, kehadiran web developer tidak hanya sebagai pembuat web namun juga sebagai pemelihara sistem agar tetap berjalan lancar. Yang kedua, kemampuan Kamu dibutuhkan oleh banyak perusahaan. Mulai dari start up hingga perusahaan yang sudah berjaya puluhan tahun, membutuhkan web developer agar tetap eksis di zaman sekarang. Alasan memilih menjadi web developer berikutnya adalah karena gajinya yang besar. Sudah menjadi rahasia umum bahwa gaji web developer bisa menyentuh angka dua digit bahkan untuk lulusan baru sekalipun.</p>
+        <p >Terdapat beberapa keuntungan menjadi seorang web developer seperti Anda akan memiliki banyak kesempatan untuk bekerja dari jarak jauh, Dan mungkin Anda akhirnya dapat memenuhi impian bepergian keliling dunia, sambil menabung disaat yang bersamaan. Beberapa perusahaan Startup menerapkan hal ini, termasuk salah satunya adalah Logique Digital Indonesia, membuat web sendiri dan bekerja secara mandiri.</p>
+        </fieldset>
+        <ol>
+            <li>Pendidikan
+                <ul>
+                    <li>SDN 69 Kota Bengkulu</li>
+                    <li>SMPN 01 Kota Bengkulu</li>
+                    <li>SMAN 05 Kota Bengkulu</li>
+                    <li>Universitas Bengkulu</li>
+                </ul>
+            </li>
+            <br/>
+            <li>Hobi
+                <ul>
+                    <li>Olahraga
+                        <ul>
+                            <li>Futsal</li>
+                            <li>Badminton</li>
+                            <li>Voli</li>
+                        </ul>
+                    </li>
+                    <li>Bermain Game
+                        <ul>
+                            <li>Fifa</li>
+                            <li>Mobile Legend</li>
+                        </ul>
+                    </li>
+                    <li>Travelling</li>
+                </ul>
+            </li>
+            <br/>
+            <li>Menu Favorit
+                <ul>
+                    <li>Makanan
+                        <ol>
+                            <li>Nasi Goreng</li>
+                            <li>Pecel Lele</li>
+                            <li>Sate Madura</li>
+                        </ol>
+                    </li>
+                    <li>Minuman
+                        <ol>
+                            <li>Es teh</li>
+                            <li>Susu Coklat</li>
+                            <li>Teh susu</li>
+                        </ol>
+                    </li>
+                </ul>
+            </li>
+        </ol>
+    
+    <hr/>
+    <h3>Design By : Adam Idham Ramadhan - UNIB@2022</h3>
+    </body>
+</html>
